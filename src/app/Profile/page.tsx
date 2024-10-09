49619cb87474f4ecff13e664d090ba5b69c5bf2b
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2024-10-09 20:00:05 +0500

    Delete src/app directory

--- src/app/Profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "../Navbar/page";
-export default function Profile() {
-  return (
-    <div>
-      <h1 className="text-center">Profile</h1>
-      <p className="text-center">User profile data will be displayed here.</p>
-    </div>
-  );
-}
