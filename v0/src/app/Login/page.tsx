3dc4e3c34e890e3944067214d260739e7ac4011b
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2025-02-08 21:54:03 +0500

    Add files via upload

---
+++ v0/src/app/Login/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Login() {
+  return (
+    <div>
+      <h1 className="text-center">Login</h1>
+      <p className="text-center">Login form will be created here</p>
+    </div>
+  );
+}
---
+++ v0/src/app/Profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1 className="text-center">Profile</h1>
+      <p className="text-center">User profile data will be displayed here.</p>
+    </div>
+  );
+}
